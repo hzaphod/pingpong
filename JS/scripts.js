0d4bc0685f07007a64493e046539161c601c9282
Heather <newwavehealing@gmail.com>
2016-03-15 15:20:31 -0700

    Add initial business logic to scripts.js

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,4 +1,10 @@
+//A loop is needed to take a user input then return/write/show all numbers leading up to that input.
+
 var pingPong = function(inputNumber) {
   for (index = 0; index >=inputNumber; index -=1)
     console.log(index);
 }
+
+//Modify results to show returned numbers in an unordered list
+
+//Further modify to show any result where inputNumber % 3 === 0 returns the number 3, or inputNumber % 5 === 0 returns pong, or inputNumber % 15 === 0 returns pingpong.
