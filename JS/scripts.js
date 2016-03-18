3d1dd36018cd55bd65fa20ce65f794ac4b1a0824
Heather <newwavehealing@gmail.com>
2016-03-17 22:48:29 -0700

    Add remaining logic to scripts so that modulo math is calculated and displayed, results are shown, and form can reset. also updates html so that the page does not refresh upon submit.

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,13 +1,31 @@
+$(document).ready(function() {
 //A loop is needed to take a user input then return/write/show all numbers leading up to that input.
-
-var pingPong = function(inputNumber) {
-  for (index = inputNumber; index >= 1; index -=1)
-    console.log(index);
-}
-
-//Modify results to show returned numbers in an unordered list
-
 //Further modify to show any result where inputNumber % 3 === 0 returns the number 3, or inputNumber % 5 === 0 returns pong, or inputNumber % 15 === 0 returns pingpong.
 //if (index % 3 ===0) {
   //console.log("ping");
 //}
+
+  var pingPong = function(inputNumber) {
+    for (index = inputNumber; index >= 1; index -=1) {
+      if (index % 15 === 0)
+        showResult("pingpong");
+      else if (index % 5 === 0)
+        showResult("pong");
+      else if (index % 3 === 0)
+        showResult("ping");
+      else
+        showResult(index);
+    }
+  }
+  //Modify results to show returned numbers in an unordered list
+  var showResult = function(input) {
+    $("#results").prepend("<li>" + input);
+  }
+
+//Add click function to show and reset results
+
+  $('#mySubmit').click(function() {
+    $("#results").html("");
+    pingPong($("#userInput").val());
+  });
+});
